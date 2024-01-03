064209ca01ac4ef4f65d97042ff4647965746a32
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-03 14:57:12 +0100

    footer

---
+++ resources/views/_footer.blade.php
@@ -0,0 +1,9 @@
+    <footer class=" bg-dark bg-opacity-75 p-2 fw-light text-white">
+        <div>
+            Car Rental Management
+            <p class="text-center mt-3">Copyright@2024 Tous droit réservé</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
