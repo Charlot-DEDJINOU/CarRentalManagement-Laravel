9dc54a88e0a0739adbc976e8e2e257cef27e25a8
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-03 14:57:37 +0100

    header

---
+++ resources/views/_header.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html ang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Car Rental Management</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand mx-3 fs-5 text-white fw-medium" href="#">Car Rental Management</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse"></div>
+          <div class="collapse navbar-collapse"></div>
+          <div class="collapse navbar-collapse"></div>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active fs-5 text-white fw-light mx-3" href="#">Acceuil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5 text-white fw-light mx-3" href="#">Nos voitures</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5 text-white fw-light mx-3" href="#">Contact</a>
+              </li>
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active fs-5 text-white fw-light mx-3" href="#">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5 text-white fw-light mx-3" href="#">Inscription</a>
+                </li>
+            </ul>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    
