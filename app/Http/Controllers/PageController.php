8a1240d80c744ea0419a57abc2c2504057aa9eb7
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-05 16:44:58 +0100

    Controller for static pages

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function acceuil() {
+        return view('welcome');
+    }
+
+    public function contact() {
+        return view('contact');
+    }
+
+    public function login() {
+        return view('login');
+    }
+
+    public function register() {
+        return view('register');
+    }
+
+    public function cars() {
+        return view('cars');
+    }
+}
