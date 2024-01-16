1d7fa56cbb2399e6e6a4a2e9e948d582fddae6c8
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-16 13:11:10 +0100

    use Authenticatable model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
+    
+    protected $fillable = ['name', 'email', 'password', 'admin'];
+
     public function rentals()
     {
         return $this->hasMany(Rental::class);
