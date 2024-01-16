49dcf12568e59b5d0a056b16f8bdd4820a085740
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-01-16 13:10:28 +0100

    fonctions register , Login , logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,28 +1,61 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function inscription(Request $request)
+    public function register(Request $request)
     {
-        $request->validate([
-            'username' => ['required', 'string', 'max:255', Rule::unique('users')],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+       
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|required',
+            'email' => 'email|required|unique:users,email',
+            'password' => 'required|confirmed'
         ]);
+        
+        if ($validator->fails()) {
+            return view('register', [
+                'error' => true,
+                'nameError' => $validator->errors()->first('name'),
+                'emailError' => $validator->errors()->first('email'),
+                'passwordError' => $validator->errors()->first('password'),
+            ]);
+        }        
 
-        $user = User::create([
-            'name' => $request->input('username'),
+        // Création d'un nouvel utilisateur dans la base de données
+        User::create([
+            'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
         ]);
 
-        return redirect()->route('acceuil')->with('success', 'Inscription réussie ! Connectez-vous maintenant.');
+        return redirect()->route('login_page')->with('success', 'Inscription réussie ! Connectez-vous maintenant.');
     }
 
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+            return redirect()->route('acceuil')->with('success', 'Connexion réussie.');
+        }
+
+        return redirect()->route('login_page')->with('error', 'E-mail ou mot de passe incorrect.');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('acceuil')->with('success', 'Vous avez été déconnecté.');
+    }
 }
